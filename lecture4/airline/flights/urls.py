c8b65c7dc483fec9729f1089a2ba12335023c180
PedroAraujooj <111099078+PedroAraujooj@users.noreply.github.com>
2023-07-17 23:47:45 -0300

    add passenger url

--- lecture4/airline/flights/urls.py
+++ lecture4/airline/flights/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns=[
     path("", views.index, name="index"),
-    path("<int:flightId>", views.flight, name="flight")
+    path("<int:flightId>", views.flight, name="flight"),
+    path("<int:flightId/book>", views.book, name="book")
 ]
--- lecture4/airline/flights/views.py
+++ lecture4/airline/flights/views.py
@@ -1,5 +1,8 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from .models import Flight
+from django.urls import reverse
+
+from .models import *
 # Create your views here.
 def index(request):
     return render(request, "flights/index.html", {
@@ -10,5 +13,13 @@ def index(request):
 def flight(request, flightId):
     flightItem = Flight.objects.get(pk=flightId)
     return render(request, "flights/flight.html", {
-        "flight":flightItem
+        "flight": flightItem,
+        "passengers": flightItem.passengers.all()
     })
+
+def flight(request, flightId):
+    if request.method == "POST":
+        flight = Flight.objects.get(pk = flightId)
+        passenger = Passenger.objects.get(pk = int(request.POST["passenger"]))
+        passenger.flights.add(flight)
+        return HttpResponseRedirect(reverse("flight", args=flight.id))
