21cbe5dea6c9fd582219ed01be7ca0126b26d504
PedroAraujooj <111099078+PedroAraujooj@users.noreply.github.com>
2023-07-17 21:03:39 -0300

    admin user

--- lecture4/airline/flights/views.py
+++ lecture4/airline/flights/views.py
@@ -5,3 +5,10 @@ def index(request):
     return render(request, "flights/index.html", {
         "flights": Flight.objects.all()
     })
+
+
+def flight(request, flightId):
+    flightItem = Flight.objects.get(pk=flightId)
+    return render(request, "flights/flight.html", {
+        "flight":flightItem
+    })
